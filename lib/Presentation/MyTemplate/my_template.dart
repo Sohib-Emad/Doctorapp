2f838a29402017891bf06129aad12c6d760f0d39
Sohib-Emad <m9749172@gmail.com>
2026-04-26 19:42:10 +0300

    feat: Add MyTemplate with custom header and tabbed content for medicine and lab tests
    - Created MyTemplate widget as the main structure for the template.
    - Implemented CustomHeaderTemplate with tab navigation for Medicine and Lab Test sections.
    - Added LabTemplate and MedicineTemplate widgets for respective content.
    - Introduced PatineWidgetsTemplate for displaying patient information with edit and delete options.

---
+++ lib/Presentation/MyTemplate/my_template.dart
@@ -0,0 +1,31 @@
+import 'package:doctor/Core/Theme/color_app.dart';
+import 'package:doctor/Core/helper/image_assets.dart';
+import 'package:doctor/Presentation/MyTemplate/widgets/custom_header_template.dart';
+import 'package:doctor/Presentation/MyTemplate/widgets/medicine_template.dart';
+import 'package:doctor/Presentation/MyTemplate/widgets/patine_widgets_template.dart';
+import 'package:doctor/widgets/Add_prescription/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class MyTemplate extends StatelessWidget {
+  const MyTemplate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorApp.scaffoldColor,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(ImageAssets.backgroundImagePath2),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: [const CustomHeaderTemplate(title: 'My Template')],
+          ),
+        ),
+      ),
+    );
+  }
+}
